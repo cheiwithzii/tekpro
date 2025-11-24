209d216053749b01b6c849339867e932f78f33c9
Grace Sheilla Simbolon <grace.124120036@student.itera.ac.id>
2025-11-24 19:39:48 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,27 @@ if "user_id" not in st.session_state:
 # Optional: tampilkan session di dashboard
 st.write(f"Session User ID: {st.session_state.user_id}")
 
+background_url = "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?auto=format&fit=crop&w=1350&q=80"
+
+st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        /* Background image */
+        background-image: url("{background_url}");
+        background-size: cover;
+        background-attachment: fixed;
+        background-repeat: no-repeat;
+        background-position: center;
+
+        /* Fallback color */
+        background-color: #ffc0cb;  /* pink pastel */
+        height: 100vh;
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+
 # ============================
 # 1. Konfigurasi & Fungsi Utils
 # ============================
